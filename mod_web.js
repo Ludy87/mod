78c271aabfe4a59fd10a0e8c5e480251b9e0998e
Ludy87 <Ludy87@users.noreply.github.com>
2016-08-01 20:10:10 +0200

    Update mod_web.js

--- mod_web.js
+++ mod_web.js
@@ -226,11 +226,7 @@ function Android () {
 		var mThis = this;
 		console.log(mThis);
 		var reply = tld + "_reply";
-		console.log("-->" + chrome.i18n.getMessage(tld + "_reply"))
-		console.log("-->" + chrome.i18n.getMessage(reply))
-		console.log("-->" + tld)
-		//de_reply
-		if($(this).text().trim() == chrome.i18n.getMessage(tld + "_reply")) {
+		if($(this).text().trim() == chrome.i18n.getMessage(reply)) {
 			$().ready(function() {
 				setTimeout(writeSite, 1500);
 			});
