7454ceeb6c96c5e8d35a8c0c7ab7a1f8432ef630
Ludy87 <Ludy87@users.noreply.github.com>
2016-08-04 20:40:53 +0200

    Update mod.js

--- javascript/web/mod.js
+++ javascript/web/mod.js
@@ -5,6 +5,7 @@ console.log(mail)
 console.log(areaUserId)
 console.log(admin)
 console.log(chrome.i18n.getMessage(tld + "_forum"))
+console.log(mail == undefined && hostPathLength == 5 && ((area == "foro") || (area == "forum")) && ((areaUserId == undefined) || (areaUserId = chrome.i18n.getMessage(tld + "_forum"))))
 
 if (mail == undefined && hostPathLength == 5 && ((area == "foro") || (area == "forum")) && ((areaUserId == undefined) || (areaUserId = chrome.i18n.getMessage(tld + "_forum")))) {
 	new Android().modPanel();
