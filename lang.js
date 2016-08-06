83edf773f5952df7eeedb5f67bca497fdfbdc3d8
Ludy87 <Ludy87@users.noreply.github.com>
2016-08-06 20:30:17 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,5 +1,34 @@
 T_language = {
 	de: {
-	  test: "test"
+	  	wrapper: 			"Moderatoren-Forum",
+		username: 			"Benutzername",
+		ip_adresse: 		"IP-Adresse",
+		manageuser: 		"User verwalten",
+		mod_panel: 			"Mod Panel",
+		close: 				"schließen",
+		warnings_user: 		"Alle Verwarnungen",
+		warn_user: 			"Verwarnen",
+		ips: 				"IP\'s",
+		adminSite_text: 	"Adminseite",
+		pm_limit_changer: 	"PM-Limit-Changer",
+		send_mail: 			"E-Mail schreiben",
+		reply_forum: 		"Antworten",
+		reply_magazin:	 	"Antworten",
+		quote: 				"Zitat",
+		default_text: 		"Standardtexte",
+		forum: 				"neue-beitraege",
+		userSearch: 		"userSearch",
+		word_filter: 		"Wortfilter Treffer aktiviert",
+		word_filter_match: 	"Treffer",
+		deleted_user: 		"Gelöschter Account",
+		trash_move: 		"Thread in den Papierkorb verschieben?",
+		reason_move: 		"Thread ist im Korb! Willst du einen Grund eingeben? Titel ist der Standardtext",
+		thread_close: 		"Thread schon geschlossen!",
+		registration:		"Registrierung ",
+		yes: 				"JA",
+		no: 				"NEIN",
+		okay: 				"Okay",
+		search:				"suche",
+		no_reply: 			"keinen Grund angeben"
 	}
 }
