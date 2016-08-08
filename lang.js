d27f6200655027dc8c00a38807dff40fe8ea266b
Ludy87 <Ludy87@users.noreply.github.com>
2016-08-08 04:47:27 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,5 +1,37 @@
 T_language = {
 	de: {},
 	br: {},
-	com: {}
+	com: {},
+	default: {
+		wrapper: "",
+		username: "",
+		ip_adresse: "",
+		manageuser: "",
+		mod_panel: "",
+		close: "",
+		warnings_user: "",
+		warn_user: "",
+		ips: "",
+		adminSite_text: "",
+		pm_limit_changer: "",
+		send_mail: "",
+		reply_forum: "",
+		reply_magazin: "",
+		quote: "",
+		default_text: "",
+		forum: "",
+		userSearch: "",
+		word_filter: "",
+		word_filter_match: "",
+		deleted_user: "",
+		trash_move: "",
+		reason_move: "",
+		thread_close: "",
+		registration: " ",
+		yes: "",
+		no: "",
+		okay: "",
+		search:	"",
+		no_reply: ""
+	}
 }
