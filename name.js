d9d2c5691af0bc862f51353909c4e8c7615afce0
Ludy87 <Ludy87@users.noreply.github.com>
2016-08-09 20:06:29 +0200

    Update name.js

--- name.js
+++ name.js
@@ -1,4 +1,4 @@
 
 if(userId == "4361215" || userId == "2927890") {
-	$(this).find(".threadPostAuthorName .user-badges span").first().append("<span> DEV</span>");
+	$(this).find(".threadPostAuthorName .user-badges span").first().append("<span> developer</span>");
 }
